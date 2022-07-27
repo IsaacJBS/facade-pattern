5b6846ecb41b7cb0e600e3ab1919d16923aced92
Isaac <isaac.jordaobs@gmail.com>
2022-07-27 16:01:25 -0300

    feat: list of sw characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+import getFetch from "./utils/getFetch";
+
 function App() {
+  const [responseData, setResponseData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function getResponse() {
+    try {
+      setLoading(true);
+      const { results } = await getFetch("people/");
+      setResponseData(results);
+    } catch (error) {
+      throw new Error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getResponse();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Oi</h1>
+      <ul>
+        {loading && <p>Carregando...</p>}
+        {responseData &&
+          responseData.map((character) => (
+            <li key={character.name}>{character.name}</li>
+          ))}
+      </ul>
     </div>
   );
 }
--- src/utils/getFetch.js
+++ src/utils/getFetch.js
@@ -1,10 +1,6 @@
 import axios from "../api/axios";
 
 export default async function getFetch (url) {
-  try {
     const response = await axios.get(url)
     return response.data
-  } catch (error) {
-    return error
-  }
 }
